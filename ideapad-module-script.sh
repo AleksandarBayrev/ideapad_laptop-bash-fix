966aa6c57338358f64f1c27692fd07f02f677d92
Aleksandar Bayrev <aleksandar_sb@abv.bg>
2017-09-21 13:19:21 +0300

    Update script for ideapad laptops

--- ideapad-module-script.sh
+++ ideapad-module-script.sh
@@ -2,13 +2,3 @@
 sudo modprobe -r ideapad_laptop
 sudo modprobe ideapad_laptop
 rfkill unblock all
-sudo modprobe -r ideapad_laptop
-sudo modprobe ideapad_laptop
-rfkill unblock all
-sudo modprobe -r ideapad_laptop
-sudo modprobe ideapad_laptop
-rfkill unblock all
-sudo modprobe -r ideapad_laptop
-sudo modprobe ideapad_laptop
-rfkill unblock all
-sudo powertop --auto-tune
